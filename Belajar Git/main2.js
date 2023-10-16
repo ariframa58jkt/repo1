cad8693eeca8c717be32579902af5252a43d8c0f
ariframa58jkt <ariframa170294@gmail.com>
2023-10-16 22:21:45 +0700

    tugas html dan css

---
+++ Belajar Git/main2.js
@@ -0,0 +1,37 @@
+const dataMahasiswa = [
+    {
+        id: 1,
+        nama: "Alice",
+        usia: 20,
+        jurusan: "Informatika"
+    },
+    {
+        id: 2,
+        nama: "Bob",
+        usia: 22,
+        jurusan: "Teknik Elektro"
+    },
+    {
+        id: 3,
+        nama: "Carol",
+        usia: 21,
+        jurusan: "Desain Grafis"
+    },
+    {
+        id: 4,
+        nama: "Balmon",
+        usia: 21,
+        jurusan: "Informatika"
+    }
+]
+
+console.log = dataMahasiswa.nama(id)
+
+
+//1. Buatkan data diatas menjadi seperti ini ["Alice","Bob","Carol"]
+//2. Buatkan array object yang hanya menampilkan nama dan usia saja
+//3. Tampilkan pada html dengan tampilan seperti ini
+// 1. nama: Alice - 20
+// 2. nama: Alice - 20
+
+
